0558f29a81c727753ef9eb4ed638fb1d66d018c7
vvsugrevin <vvsugrevin@gmail.com>
2022-02-07 17:35:22 +0300

    task 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int a=13, b=13;
+//if (a>b) Console.WriteLine("а больше b");
+//else Console.WriteLine("а не больше b");
+if (a>b) Console.WriteLine("а больше b");
+if (a<b) Console.WriteLine("а меньше b");
+if (a==b) Console.WriteLine("а равно b");
