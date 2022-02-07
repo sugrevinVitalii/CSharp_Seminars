8905797e5481eb3e38227d9f2949e9b8c703cc43
vvsugrevin <vvsugrevin@gmail.com>
2022-02-07 21:13:58 +0300

    Zadacha 6

---
+++ 6/Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+void Pause(string message="Press any key")//описание подпрограммы (метода), аналог команды goto
+{
+    System.Console.WriteLine(message);
+    Console.ReadKey(); //Команда останавливает выполнение УП до нажатия любой клавиши
+}
+
+
+Pause();//вызов. т.е. идет вызов метода (Console.ReadKey();)и его выполнение.
