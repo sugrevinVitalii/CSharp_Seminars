f6fff1f8161a3cea2529906f6213e53f14a4d74d
vvsugrevin <vvsugrevin@gmail.com>
2022-02-13 21:05:57 +0300

    task of string, задача про строки.

---
+++ Str01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дана строка текста, надо ее перевернуть.
+
+string s=Console.ReadLine();
+string s1 = String.Empty;
+
+for (int i = s.Length-1; i>=0; i--)
+{
+    s1=s1+s[i];//склеивание или конкатинация строк.
+    //System.Console.Write(s[i]);
+}
+System.Console.WriteLine(s1);
