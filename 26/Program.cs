a0f6e3a71fdd0145dbd279b7abd5e1fac1cab652
vvsugrevin <vvsugrevin@gmail.com>
2022-02-17 22:27:29 +0300

    Task 26

---
+++ 26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int A = 15;
+int B = 2;
+int result = 1;
+
+for (int i=1; i<=B; i++)
+{
+    result = result * A;
+}
+System.Console.WriteLine(result);
