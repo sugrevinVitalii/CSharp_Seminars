222d6d93c0ad27bf9648fcc987be7ebb98b8ff24
vvsugrevin <vvsugrevin@gmail.com>
2022-02-21 22:20:56 +0300

    Шаблон на переменные

---
+++ Add02/Program.cs
@@ -0,0 +1,11 @@
+﻿// ввести два числа с клавиатуры.
+
+int a,b;//Глобальные переменные
+
+void Input(out int x, out int y)
+{
+    x=int.Parse(Console.ReadLine());
+    y=int.Parse(Console.ReadLine());
+}
+
+Input(out a, out b);
