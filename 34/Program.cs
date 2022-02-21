8b52932da7915511ff37fd8b6bb0546f2c866a5a
vvsugrevin <vvsugrevin@gmail.com>
2022-02-21 21:42:14 +0300

    Task 34 and shablon

---
+++ 34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу замены элементов массива на противоположные.
+
+int N=10;
+int[] array=new int[N];
+
+void Input(int[] arr) //Создание рандомного массива
+{
+    Random random=new Random();//Создали объект класса Random и назвали его ransom
+    for(int i=0; i<arr.Length; i++)
+        arr[i]=random.Next(1,100);
+}
+void Solve(int[] arr)//Обработка данных
+{
+    for(int i=0;i<arr.Length;i++)
+        arr[i]=-arr[i];
+}
+
+void Print(int[] arr) //Вывод результата.
+{
+    for(int i=0; i<arr.Length; i++)
+        System.Console.Write($"{arr[i]} ");
+}
+
+Input(array);
+Print(array);
+Solve(array);
+System.Console.WriteLine();
+Print(array);
---
+++ add01/Program.cs
@@ -0,0 +1,21 @@
+﻿// НАйти сумму двух чисел и вывести ее на экран. OldSchool
+
+//C# 10 (NetCore 6)
+//int a=1,b=2;
+//int sum=a+b;
+//System.Console.WriteLine(sum);
+
+//C# 9 (NetCore 5)
+
+namespace Petrov
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a=1, b=2;
+            int sum=a+b;
+            System.Console.WriteLine(sum);
+        }
+    }    
+}
