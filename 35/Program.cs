f37f0d3622f5784913e07e13aeab3594c1c507f4
vvsugrevin <vvsugrevin@gmail.com>
2022-02-21 22:56:01 +0300

    Task 35

---
+++ 35/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число.
+
+int [] arr={22, 33,44,55,66,77,88};
+int InputN()
+{
+    string s;
+    System.Console.WriteLine(" Enter a number to search");
+    s=Console.ReadLine();
+    return Convert.ToInt32(s);
+}
+int n=InputN ();
+bool flag = false;
+int i;
+for (i=0; i<arr.Length; i++)
+{
+    if (arr[i]==n)
+    {
+        flag=true;
+        break;
+    }
+}
+if(flag==true)
+    System.Console.WriteLine(" The entered number is present in the array {0}, Serial number {1} ", n, i);
+else 
+    System.Console.WriteLine( " the entered number is not in the array ");
