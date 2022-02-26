56120de5a5d2d5bc2bdba9a7ef98afac75d996d9
vvsugrevin <vvsugrevin@gmail.com>
2022-02-26 09:19:26 +0300

    Task 5

---
+++ 5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел
+int a = 11;
+int b = 22;
+int c = 14;
+int max = a;
+
+if (a >= max) max = a;
+if (b >= max) max = b;
+if (c >= max) max = c;
+   
+System.Console.WriteLine($"Максимальное число:{max}");
