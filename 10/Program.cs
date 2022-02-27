3c7d5b67055a5a60166158a639d8c0b9f083180f
vvsugrevin <vvsugrevin@gmail.com>
2022-02-27 19:32:51 +0300

    Task 10

---
+++ 10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int a = 0;
+string s = string.Empty;
+
+System.Console.Write("Введите трехзначное число ");
+s = Console.ReadLine();
+a = s.Length;
+
+switch (a)
+{
+    case 3:
+        Console.WriteLine("Последняя цифра трехзначного числа " + s[s.Length - 1]);
+        break;
+    default:
+        Console.WriteLine("Число не является трехзначным");
+        break;
+}
