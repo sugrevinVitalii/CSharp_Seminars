a502ed95440103a75ee9689cecff991834121166
vvsugrevin <vvsugrevin@gmail.com>
2022-02-27 15:37:53 +0300

    Task 9

---
+++ 9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+
+int a = 0;
+
+System.Console.WriteLine("Enter the end number of the sequence ");
+string s = string.Empty;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int i = 1; i <= a; i++)
+{
+    if(i%2 == 0) System.Console.Write(i + " ");
+}
