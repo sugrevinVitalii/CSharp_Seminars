ecc4f736ca7f0e94b78ecb23d89b1e55fbe176f9
vvsugrevin <vvsugrevin@gmail.com>
2022-03-08 20:42:40 +0300

    Task 12

---
+++ 12/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10,100);
+int [] arr = new int[2];
+int max = 0;
+
+System.Console.WriteLine("Случайное число из отрезка (10 - 99) " + a);
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = a%10;
+        a = a/10;
+    }
+
+max = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] > max) max = arr[i];
+}
+
+System.Console.WriteLine("Наибольшая цифра в числе " + max);
