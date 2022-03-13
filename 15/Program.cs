f8b54ef95cb0e15957b8a812079cf40f766de0b7
vvsugrevin <vvsugrevin@gmail.com>
2022-03-13 21:03:41 +0300

    Task 15

---
+++ 15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int a = 0;
+string s = string.Empty;
+
+System.Console.Write("Введите положительное число ");
+s = Console.ReadLine();
+a = s.Length;
+
+if(a >= 3) 
+{
+    System.Console.WriteLine("Третья цифра у числа " + s[2]);
+}
+else System.Console.WriteLine("В числе нет третьей цифры.");
