61b68b8a6bec73989ed778e418acd393d4b98e02
vvsugrevin <vvsugrevin@gmail.com>
2022-03-19 12:49:30 +0300

    Task 24

---
+++ 24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+int a = 0;
+
+System.Console.WriteLine("Введите число: ");
+string s = string.Empty;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+System.Console.WriteLine($"Кубы чисел от 1 до {s} :");
+for (int i = 1; i <= a; i++)
+{
+    System.Console.Write(i*i*i + " ");
+}
