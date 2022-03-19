6157376ab6d9ec07cd6edb0a0cdcf596ea8683be
vvsugrevin <vvsugrevin@gmail.com>
2022-03-19 12:46:19 +0300

    Task 23

---
+++ 23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+int a = 0;
+
+System.Console.WriteLine("Введите число: ");
+string s = string.Empty;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+System.Console.WriteLine($"Квадраты чисел от 1 до {s} :");
+for (int i = 1; i <= a; i++)
+{
+    System.Console.Write(i*i + " ");
+}
