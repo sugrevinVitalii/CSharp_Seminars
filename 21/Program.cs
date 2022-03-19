984cb18f2bf689322aaf2439dde32eb02a8db6de
vvsugrevin <vvsugrevin@gmail.com>
2022-03-19 12:35:31 +0300

    Task 21

---
+++ 21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа проверяет пятизначное число на палиндром
+
+int a = 0;
+string s = string.Empty;
+
+System.Console.Write("Введите положительное пятизначное число: ");
+s = Console.ReadLine();
+a = s.Length;
+//s=Math.Abs(s);
+
+if(a == 5)
+{
+    int [] array = new int[a];
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = Convert.ToInt32(s[i]);
+    }
+    if(array[0] == array[4] && array[1] == array[3])
+    {
+        System.Console.WriteLine($"Введенное число {s} является полиндромом.");
+    }
+    else System.Console.WriteLine($"Введенное число {s} не является полиндромом.");
+}
+else System.Console.WriteLine($"Число {s} не является пятизначным или положительным.");
